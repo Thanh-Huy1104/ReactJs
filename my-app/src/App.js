5920ad935aaeaf495390bba2964225b46cf899a5
TH <etienne01.nguyen@gmail.com>
2022-09-29 16:39:57 -0400

    modified site

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,27 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import TopSite from './components/TopSite/TopSite';
+import FootSite from './components/FootSite/FootSite';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <TopSite></TopSite>
+      <FootSite></FootSite>
     </div>
   );
-}
+};
 
-console.log("hello world")
+console.log("Testing is working!")
 
 export default App;
---
+++ my-app/src/components/FootSite/FootSite.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './FootSite.css';
+
+class FootSite extends React.Component{
+    render() {
+      return (
+      <div className="footer"> This remains at the bottom!
+      </div>
+      )
+    };
+  };
+  
+  export default FootSite;
