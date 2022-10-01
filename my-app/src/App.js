4380132a80c3defa964c6f14aafc19ab48e92b35
TH <etienne01.nguyen@gmail.com>
2022-09-30 23:42:09 -0400

    all new things to site

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 import TopSite from './components/TopSite/TopSite';
 import FootSite from './components/FootSite/FootSite';
+import reload from './components/svgs/Logo.svg'
+import React from 'react';
 
 function App() {
   return (
     <div>
+      <header className='heading'>
+      <a href='./'><img src={reload} alt='reloading page' className='logo'></img></a>
+      </header>
       <TopSite></TopSite>
       <FootSite></FootSite>
     </div>
--- my-app/src/components/FootSite/FootSite.js
+++ my-app/src/components/FootSite/FootSite.js
@@ -4,7 +4,7 @@ import './FootSite.css';
 class FootSite extends React.Component{
     render() {
       return (
-      <div className="footer"> This remains at the bottom!
+      <div className="footer"> <a href='mailto:etienne01.nguyen@gmail.com'>Get in touch</a>
       </div>
       )
     };
