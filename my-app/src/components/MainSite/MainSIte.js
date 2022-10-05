0b03b4aee993afe266dce6376326bbd03f1754af
TH <etienne01.nguyen@gmail.com>
2022-10-05 00:32:55 -0400

    added left bar

---
+++ my-app/src/components/MainSite/MainSIte.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './MainSite.css';
+
+
+class MainSite extends React.Component{
+  render() {
+    return (
+    
+    )
+  };
+};
+
+export default MainSite;
--- my-app/src/components/TopSite/TopSite.js
+++ my-app/src/components/TopSite/TopSite.js
@@ -1,7 +1,6 @@
 import React from "react";
 import './TopSite.css';
 
-
 class TopSite extends React.Component{
   render() {
     return (
